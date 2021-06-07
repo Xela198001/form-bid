b9acecfd4d32744e757de542c0a30f295d82034f
Aleksandr Meshcheryakov <mes-aleksandr@yandex.ru>
2021-06-07 13:04:40 +0300

    add adaptive

--- js/country.js
+++ js/country.js
@@ -1,8 +1,15 @@
-const countryElem = document.querySelector('#country-phone');
+const countryElem = document.querySelector('.arrow');
 const countryList = document.querySelector(".country-list");
 
+
 function countryListShow(event) {
-  countryList.classList.add('.show');
+  countryList.classList.toggle('show');
+  countryList.addEventListener('click', function listHide(event) {
+    const target = event.target;
+    if (target.matches('.country-item')) { countryList.classList.remove("show"); }
+    countryList.removeEventListener('click', listHide);
+  })
+  
 }
 
 countryElem.addEventListener('click', countryListShow, true);
